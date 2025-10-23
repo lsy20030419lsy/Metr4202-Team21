b807fe1ea6dd37d794047286f2870d9620c160c9
StevenMa-creater <yimingma89@gmail.com>
2025-10-23 15:49:50 +1000

    Add aruco_detector_team21 and waypoint_commander_team21 packages

---
+++ waypoint_commander_team21/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'waypoint_commander_team21'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='will',
+    maintainer_email='will@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [ 'waypoint_cycler_team21 = waypoint_commander_team21.waypoint_cycler_team21:main',
+
+        ],
+    },
+)
